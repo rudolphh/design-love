d631e98689a25da7db362d5d3ad75b961a58c0a0
Rudy Hernandez Jr <rudolpharthur@gmail.com>
2020-11-19 13:44:15 -0800

    Update index.js
    Quotes On Design API changed, queries updated.

--- js/index.js
+++ js/index.js
@@ -18,45 +18,47 @@ function PopupCenter(url, title, w, h) {
     }
 }
 
-$(document).ready(function () {
-  $.ajaxSetup({ cache: false });
 
-  function decodeHtml(html) {
-    var txt = document.createElement("textarea");
-    txt.innerHTML = html;
-    return txt.value;
-  }
+$(document).ready(function(){ 
+    $.ajaxSetup({ cache: false });
+  
+    function decodeHtml(html) {
+        var txt = document.createElement("textarea");
+        txt.innerHTML = html;
+        return txt.value;
+    }
+  
+    function setQuotes(a){
+    
+        let newQuote = $(a[0].content.rendered).text();
+        let newAuthor = a[0].title.rendered;
+        $("#text").html(newQuote);
+        $("#author").text("- " + decodeHtml(decodeURIComponent(newAuthor)));
+    
+        $('#tweet-quote').attr('href', 'https://twitter.com/intent/tweet?hashtags=quotes&via=rudolphharthur&related=rudolphharthur&text=' + encodeURIComponent('"' + newQuote + '" ' + newAuthor));
+        $("#quote-box").css("display", "block");
+        setColors();
+    }// end setQuotes
+  
+    function getRandomColor(){
+        return 'rgb('+ (Math.floor(Math.random() * 256)) +','+ 
+                         (Math.floor(Math.random() * 256)) +','+ 
+                         (Math.floor(Math.random() * 256)) +')';
+    }
+  
+    function setColors(){
+        let newColor = getRandomColor();
+        $("body").css({ backgroundColor: newColor });
+        $(".change-background-color").css({ backgroundColor : newColor });
+        $(".change-color").css({ color : newColor });
+  
+    }
+  
+    $.getJSON("https://quotesondesign.com/wp-json/wp/v2/posts?orderby=rand&posts_per_page=1&callback=", setQuotes);
 
-  function setQuotes(a) {
-
-    var newQuote = $(a[0].content).text();
-    var newAuthor = a[0].title;
-    $("#text").html(newQuote);
-    $("#author").text("- " + decodeHtml(decodeURIComponent(newAuthor)));
-
-    $('#tweet-quote').attr('href', 'https://twitter.com/intent/tweet?hashtags=quotes&via=rudolphharthur&related=rudolphharthur&text=' + encodeURIComponent('"' + newQuote + '" ' + newAuthor));
-    $("#quote-box").css("display", "block");
-    setColors();
-  } // end setQuotes
-
-  function getRandomColor() {
-    return 'rgb(' + Math.floor(Math.random() * 256) + ',' + Math.floor(Math.random() * 256) + ',' + Math.floor(Math.random() * 256) + ')';
-  }
-
-  function setColors() {
-    var newColor = getRandomColor();
-    $("body").css({ backgroundColor: newColor });
-    $(".change-background-color").css({
-      backgroundColor: newColor });
-    $(".change-color").css({
-      color: newColor
+    $('#new-quote').on('click', function(e) {
+        e.preventDefault();
+        $.getJSON("https://quotesondesign.com/wp-json/wp/v2/posts?orderby=rand&posts_per_page=1&callback=", setQuotes);
     });
-  }
-
-  $.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", setQuotes);
-
-  $('#new-quote').on('click', function (e) {
-    e.preventDefault();
-    $.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", setQuotes);
-  });
-});
+    
+});// end document ready
